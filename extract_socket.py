6a5bd75277513c53b72148ef5040470da79f793b
Paulo Roberto <paulo.roberto97@live.com>
2020-08-08 12:25:41 -0300

    Adicionando funcao para pegar as FLAGS TCP

--- extract_socket.py
+++ extract_socket.py
@@ -1,9 +1,12 @@
+from sys import argv
+
+
 # Info
 mensagem = "Ferramenta para analisar bytes de rede e extrair informacoes como SOCKET(IP + PORTA) e FLAGS ativas na comunicacao"
 print("-+" * len(mensagem))
 
 # Pegando o nome do arquivo
-caminho_do_arquivo = str(input("Digite o caminho do arquivo (ex: /ubuntu/hexa_bytes.txt): ")).strip()
+caminho_do_arquivo = argv[1].strip()
 print(caminho_do_arquivo)
 
 # Variaveis de controle
@@ -13,6 +16,7 @@ ip_origem = ""
 ip_destino = ""
 porta_origem = ""
 porta_destino = ""
+tcp_flags = []
 elementos = 0
 
 with open(caminho_do_arquivo, "r" ) as arquivo:
@@ -24,7 +28,6 @@ with open(caminho_do_arquivo, "r" ) as arquivo:
 # Pegando cada lista que forma uma string e transformando cada elemento em um item da nova lista
 for qtd_itens in range(len(conteudo)):
     bytes += conteudo[qtd_itens].split()
-print(f"Numero de bytes: {len(bytes)}") 
 
 # Separando corretamente os IPs/Portas
 for byte in bytes:
@@ -51,19 +54,46 @@ for byte in bytes:
         if contador == 38:    
             porta_destino = str(int(porta_destino, 16))
             elementos += contador
-           
-            # Apresentacao do Socket
-            print("-=" * 20)
-            print(f"Socket de origem: {ip_origem}:{porta_origem}")
-            print(f"Socket de destino: {ip_destino}:{porta_destino}")
+    
+    elif contador == 48:
+        flags = int(byte, 16)
+        
+        print("-=" * 20)
+        print(f"Hexadeciaml que indica as FLAGS: 0x{flags}")
+        while flags != 0:
+            if flags >= 32:
+               tcp_flags.append("URG")
+               flags -= 32
+            elif flags >=  16:
+               tcp_flags.append("ACK")
+               flags -= 16
+            elif flags >= 8:
+               tcp_flags.append("PSH")
+               flags -= 8
+            elif flags >= 4:
+               tcp_flags.append("RST")
+               flags -= 4
+            elif flags >= 2:
+               tcp_flags.append("SYN")
+               flags -= 2
+            elif flags >= 1:
+               tcp_flags.append("FIN")
+               flags -= 1
+        
+        # Apresentacao do Socket
+        print("-=" * 20)
+        print(f"Socket de origem: {ip_origem}:{porta_origem}")
+        print(f"Socket de destino: {ip_destino}:{porta_destino}")
+        print(f"Flags TCP ativas: {tcp_flags}") 
+    
+        # Resetando os valores para comecar o proximo bloco
+        contador = 0
+        ip_origem = ""
+        ip_destino = ""
+        porta_origem = ""
+        porta_destino = ""
+        tcp_flags = tcp_flags.clear()
+        tcp_flags = []
 
-            # Resetando os valores para comecar o proximo bloco
-            contador = 0
-            ip_origem = ""
-            ip_destino = ""
-            porta_origem = ""
-            porta_destino = ""
-
-            
     elif elementos == len(bytes):
         break
